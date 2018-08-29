d3a394068cd01fdc3f8c17f570137ff89d900c6c
Kristen Shea <kstar61@live.com>
2018-08-29 17:56:49 -0500

    Add project files.

---
+++ calculate MPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calculate_MPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
