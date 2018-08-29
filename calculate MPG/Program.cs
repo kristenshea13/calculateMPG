e838cf87e2ea0340e0b461a8c3c173240fcb0587
Kristen Shea <kstar61@live.com>
2018-08-29 18:04:00 -0500

    user enters miles and gallons of gas and total MPG is calculated

--- calculate MPG/Program.cs
+++ calculate MPG/Program.cs
@@ -6,7 +6,25 @@ namespace calculate_MPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //program that asks a user for the number of miles they have driven
+            //and the amount of gas they've consumed (in gallons), and print their miles-per-gallon.
+            Console.WriteLine("Determine your miles per gallon");
+
+            Console.Write("How many miles have you driven? ");
+            int milesInt = int.Parse(Console.ReadLine());
+
+            Console.Write("How many gallons of gas have you used? ");
+            int gallonsInt = int.Parse(Console.ReadLine());
+
+            int milesperGallon = milesInt / gallonsInt;
+
+            Console.WriteLine("Total miles per gallon: {0}", milesperGallon);
+
+            Console.ReadLine();
+
+
         }
+
+
     }
 }
